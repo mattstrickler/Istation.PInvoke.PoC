ce59c2c167cb550010b4134ec3154d91851cc724
matt.strickler <matt.strickler@istation.com>
2022-11-10 13:47:18 -0600

    comment edits

--- src/LoginTokenNativeLibrary/LoginToken.cpp
+++ src/LoginTokenNativeLibrary/LoginToken.cpp
@@ -16,6 +16,8 @@
 
 LoginToken::LoginToken(const char *token)
 {
+    // this is where parseToken would be called to parse and populate the Login Token respective properties.
+
     _token = token;
     _userOid = 123;
     _tokenOid = 456;
@@ -24,6 +26,8 @@ LoginToken::LoginToken(const char *token)
 
 LoginToken::LoginToken(long userOid, long tokenOid, long issuedDateSeconds)
 {
+    // this is where issueLoginToken AND parseToken would called to populate the LoginToken class and properties.
+
     _token = "Token2";
     _userOid = userOid;
     _tokenOid = tokenOid;
