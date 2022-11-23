1c73c787e0b0c616f15af9ec78ab70e2f199262e
matt.strickler <matt.strickler@istation.com>
2022-11-23 13:08:15 -0600

    Cleanup code for Demo

---
+++ src/CoreNativeTest/CoreNativeTest/LoginTokenBasic.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace CoreNativeTest
+{
+    public class LoginTokenBasic
+    {
+        /// <summary>
+        /// Name of Unmanaged Library (C++) being imported
+        /// </summary>
+        const string DLL_LIBRARY_BASIC = "LoginTokenNativeLibrary";
+        private readonly IntPtr _loginTokenPointer;
+
+        #region Extern Code
+        // Import unmanaged code, .dll, .so
+        [DllImport(DLL_LIBRARY_BASIC, 
+                   CharSet = CharSet.Ansi, 
+                   CallingConvention = CallingConvention.StdCall, 
+                   SetLastError = true)]
+        // The C# method which corresponds to the native unmanaged C++ code
+        public static extern IntPtr CreateLoginToken([MarshalAs(UnmanagedType.LPStr)] string token);
+
+        // Import unmanaged code, .dll, .so
+        [DllImport(DLL_LIBRARY_BASIC)]
+        // The C# method which corresponds to the native unmanaged C++ code
+        public static extern long GetUserOid(IntPtr token);
+
+        #region Collapsed for Demo
+        [DllImport(DLL_LIBRARY_BASIC)]
+        public static extern void DeleteLoginToken(IntPtr token);
+
+        [DllImport(DLL_LIBRARY_BASIC)]
+        public static extern long GetTokenOid(IntPtr token);
+
+        [DllImport(DLL_LIBRARY_BASIC)]
+        public static extern long GetIssuedDateSeconds(IntPtr token);
+
+        [DllImport(DLL_LIBRARY_BASIC, CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr GetToken(IntPtr token);
+        #endregion Collapsed for Demo
+
+        #endregion Extern Code
+
+        public LoginTokenBasic(string token)
+        {
+            _loginTokenPointer = CreateLoginToken(token);
+            UserOid = GetUserOid(_loginTokenPointer);
+
+            #region Collapsed for Demo
+            TokenOid = GetTokenOid(_loginTokenPointer);
+            IssuedDateSeconds = GetIssuedDateSeconds(_loginTokenPointer);
+
+            IntPtr intPtr = GetToken(_loginTokenPointer);
+            Token = Marshal.PtrToStringAnsi(intPtr);
+            #endregion Collapsed for Demo
+        }
+
+        #region Properties
+        private long _UserOid;
+        public long UserOid
+        {
+            get
+            {
+                return _UserOid;
+            }
+            set
+            {
+                _UserOid = value;
+            }
+        }
+
+        private long _TokenOid;
+        public long TokenOid
+        {
+            get
+            {
+                return _TokenOid;
+            }
+            set
+            {
+                _TokenOid = value;
+            }
+        }
+
+        private long _IssuedDateSeconds;
+        public long IssuedDateSeconds
+        {
+            get
+            {
+                return _IssuedDateSeconds;
+            }
+            set
+            {
+                _IssuedDateSeconds = value;
+            }
+        }
+
+        private string _Token;
+        public string Token
+        {
+            get
+            {
+                return _Token;
+            }
+            set
+            {
+                _Token = value;
+            }
+        }
+        #endregion Properties
+
+        ~LoginTokenBasic()
+        {
+            DeletePointerLoginToken();
+        }
+
+        private bool _pointerDeleted = false;
+        private void DeletePointerLoginToken()
+        {
+            if (_pointerDeleted == false)
+            {
+                DLLImports.DeleteLoginToken(_loginTokenPointer);
+                _pointerDeleted = true;
+            }
+        }
+    }
+}
--- src/CoreNativeTest/CoreNativeTest/Program.cs
+++ src/CoreNativeTest/CoreNativeTest/Program.cs
@@ -10,6 +10,12 @@ namespace CoreNativeTest
 
         private static void Main(string[] args)
         {
+            //DemoLoginToken();
+            DemoBasicLoginToken();
+        }
+
+        private static void DemoLoginToken()
+        {
             var accessor = new ResourceAccessor(Assembly.GetExecutingAssembly());
             var libManager = new LibraryManager(
                 new LibraryItem(Platform.Windows, Bitness.x64,
@@ -52,6 +58,53 @@ namespace CoreNativeTest
             var LoginToken2 = new LoginToken(10000, 12515, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds());
             Console.WriteLine();
             Console.WriteLine("Successfully created LoginToken passing UserOid, TokenOid, and IssuedAtSeconds to C++ and importing properties.");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press Any Key to Exit...");
+            Console.ReadLine();
+        }
+
+        private static void DemoBasicLoginToken()
+        {
+            var accessor = new ResourceAccessor(Assembly.GetExecutingAssembly());
+            var libManager = new LibraryManager(
+                new LibraryItem(Platform.Windows, Bitness.x64,
+                    new LibraryFile("LoginTokenNativeLibrary.dll", accessor.Binary("LoginTokenNativeLibrary.dll"))),
+                new LibraryItem(Platform.Linux, Bitness.x64,
+                    new LibraryFile("libLoginTokenNativeLibrary.so", accessor.Binary("libLoginTokenNativeLibrary.so")))
+            );
+
+            libManager.LoadNativeLibrary();
+
+            var platform = "NOT DEFINED";
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                platform = "Windows";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                platform = "Linux";
+            }
+
+            var OSArchitecture = RuntimeInformation.OSArchitecture;
+            var OSDescription = RuntimeInformation.OSDescription;
+            var FrameworkDescription = RuntimeInformation.FrameworkDescription;
+
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine("-------------------------- P-Invoke Demo ----------------------------");
+            Console.WriteLine("---------------------------------------------------------------------");
+            Console.WriteLine();
+
+            string msg = $"Platform: {platform}{Environment.NewLine}{nameof(OSArchitecture)}: {OSArchitecture}{Environment.NewLine}{nameof(OSDescription)}: {OSDescription}{Environment.NewLine}{nameof(FrameworkDescription)}: {FrameworkDescription}";
+            Console.WriteLine(msg);
+            Console.WriteLine();
+
+            var LoginToken = new LoginTokenBasic("TestToken1");
+            Console.WriteLine();
+            Console.WriteLine("Successfully created LoginToken passing token string to C++ and importing properties.");
 
             Console.WriteLine();
             Console.WriteLine();
